eb99300e850b07d26557eddb7cfe020775b2ade0
denysartiukhov <denysartiukhov@Denyss-MacBook-Pro.local>
2023-06-03 17:43:29 -0500

    incremental update

--- src/raspberry_stereo/models/MainModel.py
+++ src/raspberry_stereo/models/MainModel.py
@@ -25,7 +25,7 @@ class MainModel():
 
         rows = self.cursor.fetchall()
         if len(rows) == 0:
-            self.cursor.executescript("CREATE TABLE faces (id TEXT PRIMARY KEY, encoding TEXT NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL, creation_date DATETIME DEFAULT CURRENT_TIMESTAMP)")
+            self.cursor.executescript("CREATE TABLE faces (id TEXT PRIMARY KEY, encoding TEXT, first_name TEXT NOT NULL, last_name TEXT NOT NULL, creation_date DATETIME DEFAULT CURRENT_TIMESTAMP)")
             self.cursor.executescript("CREATE TABLE checked_in (id TEXT PRIMARY KEY, check_in_date DATETIME DEFAULT CURRENT_TIMESTAMP)")
 
             self.cursor.executescript(f"INSERT INTO faces(id, first_name, last_name) VALUES('BISERA', 'Bisera', 'Nestorovska')")
