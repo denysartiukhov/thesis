820a71f412fb3c6e1f285dca02f3ca6b6b45aaad
denysartiukhov <denysartiukhov@Denyss-MacBook-Pro.local>
2023-06-02 21:49:25 -0500

    Update main controller

--- src/raspberry_stereo/controllers/MainController.py
+++ src/raspberry_stereo/controllers/MainController.py
@@ -195,7 +195,7 @@ class MainController():
             #logging.info("info")
             time_elapsed = time.time() - prev
             mainImage = self.take_pic(self.mainCamera)
-            sideImage = self.take_pic(self.sideCamera)[0:240, 10:100]
+            sideImage = self.take_pic(self.sideCamera)[0:241, 10:100]
             print(sideImage.shape)
             self.display_pic(self.viewIdle.mainCameraLabel,mainImage)
             self.display_pic(self.viewIdle.sideCameraLabel,sideImage)
