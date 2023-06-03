7f4f3550c205fd6cb8b1813552d7ca80b8c8769a
denysartiukhov <denysartiukhov@Denyss-MacBook-Pro.local>
2023-06-03 18:11:23 -0500

    incremental update

--- src/raspberry_stereo/models/MainModel.py
+++ src/raspberry_stereo/models/MainModel.py
@@ -65,7 +65,8 @@ class MainModel():
     def getUserInfo(self, id):
         sqliteConnection = sqlite3.connect('raspberry.db')
         cursor = sqliteConnection.cursor()
-        print(id)
+        query = f"SELECT first_name FROM users WHERE id = '{id}';"
+        print(query)
         cursor.executescript(f"SELECT first_name FROM users WHERE id = '{id}';")
         rows = cursor.fetchall()
         print(rows)
