70e906029418dd5704d37c7082218fcb3790ca34
denysartiukhov <denysartiukhov@Denyss-MacBook-Pro.local>
2023-06-04 18:06:29 -0500

    incremental update

--- src/raspberry_stereo/controllers/MainController.py
+++ src/raspberry_stereo/controllers/MainController.py
@@ -427,8 +427,8 @@ class MainController():
             self.sideImage = self.take_pic(self.sideCamera,240,240)
             self.registerImage = self.sideImage
         else:
-            self.mainImage = cv2.imread(f"/home/dartiukhov/Desktop/thesis_clean/{folder}/far_straight1_c.jpg")
-            self.sideImage = cv2.imread(f"/home/dartiukhov/Desktop/thesis_clean/{folder}/far_straight2_c.jpg")
+            self.mainImage = cv2.imread(f"/home/dartiukhov/Desktop/thesis_clean/{folder}/far_straight2_c.jpg")
+            self.sideImage = cv2.imread(f"/home/dartiukhov/Desktop/thesis_clean/{folder}/far_straight1_c.jpg")
             self.registerImage = cv2.imread(f"/home/dartiukhov/Desktop/thesis_clean/{folder}/D_bright_c.jpg")
         self.display_pic(self.viewIdle.mainCameraLabel,self.mainImage)
         self.display_pic(self.viewIdle.sideCameraLabel,self.sideImage)
