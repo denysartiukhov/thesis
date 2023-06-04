a3b69e74428e36bf30a56032a9ab3e17c355e993
denysartiukhov <denysartiukhov@Denyss-MacBook-Pro.local>
2023-06-04 17:36:07 -0500

    incremental update

--- src/raspberry_stereo/controllers/MainController.py
+++ src/raspberry_stereo/controllers/MainController.py
@@ -287,15 +287,14 @@ class MainController():
         face_locations = []
         face_encodings = []
         face_names = []
-             
+  
         small_frame = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
                              
-                    # Find all the faces and face encodings in the current frame of video
+        # Find all the faces and face encodings in the current frame of video
 
         face_locations = face_recognition.face_locations(small_frame)
         face_encodings = face_recognition.face_encodings(small_frame, face_locations)
 
-        face_names = []
         for face_encoding in face_encodings:
             matches = face_recognition.compare_faces(encodings, face_encoding)
             name = "Unknown"
@@ -317,14 +316,7 @@ class MainController():
             right *= 4
             bottom *= 4
             left *= 4
-
-            # Draw a box around the face
-            #cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            # Draw a label with a name below the face
-            #cv2.rectangle(image, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            #font = cv2.FONT_HERSHEY_DUPLEX
-            #cv2.putText(image, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            
         if not face_names == []:
             return face_names[0], face_locations
         else:
