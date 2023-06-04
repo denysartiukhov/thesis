64ecb2d702aabb898b8e6f5fc0f0fb6c10d01f42
denysartiukhov <denysartiukhov@Denyss-MacBook-Pro.local>
2023-06-03 21:59:06 -0500

    incremental update

--- src/raspberry_stereo/controllers/MainController.py
+++ src/raspberry_stereo/controllers/MainController.py
@@ -198,7 +198,7 @@ class MainController():
             time_elapsed = time.time() - prev
             mainImage = cv2.imread("/home/dartiukhov/Desktop/thesis_clean/thesis/set2/Denys/bright/JPEGs/close_left1.jpg")
             #mainImage = self.take_pic(self.mainCamera,480,yMain)
-            sideImage = self.take_pic(self.sideCamera,240,240)
+            sideImage = cv2.imread("/home/dartiukhov/Desktop/thesis_clean/thesis/set2/Denys/bright/JPEGs/close_left2.jpg")
             self.show_checked_in_list()
             self.display_pic(self.viewIdle.mainCameraLabel,mainImage)
             self.display_pic(self.viewIdle.sideCameraLabel,sideImage)
