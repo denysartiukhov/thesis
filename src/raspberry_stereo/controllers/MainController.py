2f3a3948aaefe6c2e51f01bf7b7d06e49b569eb1
denysartiukhov <denysartiukhov@Denyss-MacBook-Pro.local>
2023-06-04 16:44:58 -0500

    incremental update

--- src/raspberry_stereo/controllers/MainController.py
+++ src/raspberry_stereo/controllers/MainController.py
@@ -404,15 +404,11 @@ class MainController():
                 return tuple([x, y])
     
     def estimate_direction(self,pose_estimate):
-        return "Straight"
         if float(pose_estimate[0]) > -20 and float(pose_estimate[0]) < 20 and float(pose_estimate[1]) > -20 and float(pose_estimate[1]) < 20:
-            print("Straight")
             return "Straight"
-        elif float(pose_estimate[0]) > -15 and float(pose_estimate[0]) < 15 and float(pose_estimate[1]) < -10:
-            print("Right")
+        elif float(pose_estimate[0]) > -20 and float(pose_estimate[0]) < 20 and float(pose_estimate[1]) < -10:
             return "Right"
-        elif float(pose_estimate[0]) > -15 and float(pose_estimate[0]) < 15 and float(pose_estimate[1]) > 10:
-            print("Left")
+        elif float(pose_estimate[0]) > -20 and float(pose_estimate[0]) < 20 and float(pose_estimate[1]) > 10:
             return "Left"
     
     def learn_new_face(self):
